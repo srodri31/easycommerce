941ded3eebbaade8ceab794da4c7c55d7f7ea128
Santiago Rodriguez <santi011@outlook.com>
2019-10-24 20:54:15 -0500

    adding find by id endpoint for product

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -1,5 +1,22 @@
 const Product = require("../models/product")
 
+const findById = async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id)
+        if(product) {
+            res.status(200).send(product)
+        } else {
+            res.status(404).send({
+                message: `Product with id ${req.params.id} not found.`
+            })
+        }
+    } catch(e) {
+        res.status(400).send({
+            message: e.message
+        });
+    }
+}
+
 const create = async (req, res) => {
     const product = new Product({
         ...req.body
@@ -16,5 +33,6 @@ const create = async (req, res) => {
 }
 
 module.exports = {
+    findById,
     create
 }
--- src/routers/product.js
+++ src/routers/product.js
@@ -3,6 +3,7 @@ const productController = require("../controllers/product")
 
 const router = Router()
 
+router.get("/:id", productController.findById)
 router.post("/", productController.create)
 
 module.exports = router
